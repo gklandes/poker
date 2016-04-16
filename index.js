b6ef7236d16c6074ccc61e95650747ee07e5af18
gabelandes <gabe.landes@ddifrontline.com>
2016-04-16 06:52:45 -0500

    use 'express-sse' for SSE

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var SSE = require('express-sse');
 var app = express();
 var appPort = process.argv[2] || 8080;
+var sse = new SSE(['hello']);
 
+// start the app
+app.listen(appPort, function () {
+    console.log('Poker app is up!');
+});
+
+// handle static files
 app.use(express.static('src'));
 
-app.listen(appPort, function () {
-  console.log('Poker app is up!');
-});
+// handle sse
+app.get('/sse', sse.init);
